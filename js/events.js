7b007ff7a215ae82381ae5e39304cf4b7a9b1914
Mai Campbell <jirallc@gmail.com>
2018-08-04 16:29:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,19 @@ function getIt() {
   });
 }
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('You've presses the G key);
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
