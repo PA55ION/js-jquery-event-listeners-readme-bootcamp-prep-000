c1fe8fdd573f9bc91da7cae8d01712a44ff28f63
Mai Campbell <jirallc@gmail.com>
2018-08-04 16:34:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,6 +18,12 @@ function pressIt() {
     }
   });
 }
+
+function submitIt() {
+  $('form').on('submit', function() {
+    window.alert('Your form is going to be submitted now')
+  })
+}
 $(document).ready(function(){
   getit();
   frameIt();
